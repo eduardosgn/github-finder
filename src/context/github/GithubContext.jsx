93b3a74238d1186a4a532f11a5988b28ce78a161
Eduardo <eduardosgn19@gmail.com>
2022-12-06 10:55:22 -0300

    import.meta.env

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -3,8 +3,8 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_API_URL="https://api.github.com";
-const GITHUB_TOKEN="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";
+const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
+const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
     const initialState = {
